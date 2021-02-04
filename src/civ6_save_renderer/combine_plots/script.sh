28f534232043852b94920f322ffd79f33e63bc2a
Toni Verbeiren <toni.verbeiren@gmail.com>
2021-02-04 13:19:12 +0100

    Correct combine_plots

--- src/civ6_save_renderer/combine_plots/script.sh
+++ src/civ6_save_renderer/combine_plots/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# render movie
-inputs=`echo $par_input | sed 's#:# -i #g'`
-ffmpeg -framerate $par_framerate -f image2 -i $inputs -c:v libvpx-vp9 -pix_fmt yuva420p -y $par_output
+inputs=$(echo $par_input | tr ':' '|')
+ffmpeg -framerate $par_framerate -i "concat:$inputs" -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
