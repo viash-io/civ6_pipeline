f56c363600d9b66a7034d710940fd517e6ea1a8b
Toni Verbeiren <toni.verbeiren@gmail.com>
2022-04-28 10:19:45 +0200

    Point to meta$resources_dir instead of explicit path

--- src/civ6_save_renderer/plot_map/script.R
+++ src/civ6_save_renderer/plot_map/script.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(cowplot)
 
-source(paste0("plot_map/helper.R"))
+source(paste0(meta$resources_dir, "/helper.R"))
 
 # par <- list(
 #   yaml = "/home/rcannood/workspace/di/viash_workshop_1/data.yaml",
