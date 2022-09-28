1aa355e7e1a101dca6a822cbda9d8042367ebd97
tverbeiren <tverbeiren@users.noreply.github.com>
2022-09-28 10:52:48 +0000

    deploy: 19c2a68616454c2a0dce114da1e7ecb98e1be211

--- workflows/civ6_pipeline/run.sh
+++ workflows/civ6_pipeline/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export NXF_VER=21.04.1
+export NXF_VER=22.04.5
 
 bin/nextflow \
   run . \
-  -main-script workflows/run/main.nf \
+  -main-script workflows/civ6_pipeline/main.nf \
   --input "data/*.Civ6Save" \
   --publishDir "output" \
+  -with-docker \
   -resume
